bc211460ebe443a6530ce121fd0f5d5a48fb4427
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-12 11:40:14 +0530

    Preview successfully show in the dom

--- hover-preview-accordion/src/components/Accordion.jsx
+++ hover-preview-accordion/src/components/Accordion.jsx
@@ -4,14 +4,26 @@ import { RxCross2 } from "react-icons/rx";
 
 export default function Accordion({ data }) {
     const [index, setIndex] = useState(0);
+    const [image, setImage] = useState(null);
+    const [enter, setEnter] = useState(false);
 
+    function handleEvent(image) {
+        setEnter(true)
+        setImage(image)
+    }
+    
     return (
         <div className="accordion-container">
+            {enter && <div className="preview">
+                <img src={image} alt="" />
+            </div>}
             {data.map((el) => (
                 <div className="accordion-item" key={el.id}>
                     <button
                         className="accordion-header"
                         onClick={() => setIndex(index === el.id ? null : el.id)}
+                        onMouseEnter={() => handleEvent(el.image)}
+                        onMouseLeave={() => setEnter(false)}
                     >
                         <h4>{el.title}</h4>
                         {index === el.id ? (
